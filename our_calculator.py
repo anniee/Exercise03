a59c71cde8b774f43156aabec9403f354e9cc421
Hackbright Student <info@hackbrightacademy.com>
2015-01-15 12:32:12 -0800

    works with floats

--- our_calculator.py
+++ our_calculator.py
@@ -1,41 +1,49 @@
 from arithmetic import *
+from sys import exit
 def main():
 
-    x = True 
-   # while x == True: 
+
+   while True: 
 
         cal = raw_input(">")
-        cal_split = cal.split(" ")
-        a = int(cal_split[1])
-        b = int(cal_split[2])
-      
-        if cal_split[0] == "q":
-           #x = False
-        elif cal_split[0] == "+":
-            print add(a, b)
 
-        elif cal_split[0] == "-":
-            print subtract(a, b)
-
-        elif cal_split[0] == "*":
-            print multiply(a, b)
-
-        elif cal_split[0] == "/":
-            print divide(a, b)
-
-        elif cal_split[0] == "square":
-            print square(a, b)
-
-        elif cal_split[0] == "cube":
-            print cube(a, b)
-
-        elif cal_split[0] == "pow":
-            print pow(a, b)
-
-        elif cal_split[0] == "mod":
-            print mod(a, b)
+        if cal == "q":
+            exit()
+        
         else: 
-            print "ERROR"
+            cal_split = cal.split(" ")
+            a = float(cal_split[1])
+            b = float(cal_split[2])
+
+
+            if cal_split[0] == "+":
+                print add(a, b)
+
+            elif cal_split[0] == "-":
+                print subtract(a, b)
+
+            elif cal_split[0] == "*":
+                print multiply(a, b)
+
+            elif cal_split[0] == "/":
+                if b == 0:
+                    print "ERROR: divison by 0 is not possible."
+                else:
+                    print divide(a, b)
+
+            elif cal_split[0] == "square":
+                print square(a, b)
+
+            elif cal_split[0] == "cube":
+                print cube(a)
+
+            elif cal_split[0] == "pow":
+                print pow(a, b)
+
+            elif cal_split[0] == "mod":
+                print mod(a, b)
+            else: 
+                print "ERROR"
     
 
 if __name__ == '__main__':
